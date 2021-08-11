5faab83a8d865817432cf1b732f6677b3424fadb
Nehal Sharma <68962290+N-Shar-ma@users.noreply.github.com>
2021-08-10 23:36:58 -0700

    Answer to Question 4

---
+++ BitOperations.java
@@ -0,0 +1,21 @@
+// Answer to Question 4
+
+public class BitOperations {
+    public static void main(String[] args) {
+        System.out.println("10 & 50 = " + (10&50));
+        System.out.println("30 || 40 = " + (30|40));
+        System.out.println("~50 = " + (~50));
+        System.out.println("60 >> 3 = " + (60>>3));
+        System.out.println("70 << 8 = " + (70<<8));
+        System.out.println("55 >>> 2 = " + (55>>>2));
+    }
+}
+
+// Output
+
+// 10 & 50 = 2
+// 30 || 40 = 62
+// ~50 = -51
+// 60 >> 3 = 7
+// 70 << 8 = 17920
+// 55 >>> 2 = 13
